341fb838ed6208964b3063c04c35036fe33a8281
Reggie <reggieduran15@gmail.com>
2021-05-21 16:34:51 +0800

    Added strong attack function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -6,9 +8,33 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackHandler() {
-    const damage = dealMonsterDamage(ATTACK_VALUE);
+function attackMonster(mode) {
+    let maxDamage;
+    if (mode === 'ATTACK') {
+        maxDamage = ATTACK_VALUE;
+    } else {
+        maxDamage = STRONG_ATTACK_VALUE;
+    }
+    const damage = dealMonsterDamage(maxDamage);
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
     currentMonsterHealth -= damage;
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You Lost!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!');
+    }
+}
+
+function attackHandler() {
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+    attackMonster('STRONG_ATTACK');
 }
 
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
